d26b4aa50c71b109435fb4337caa04d0e32949fb
AL1 <alibinkashif007@gmail.com>
2024-05-15 12:49:38 +0500

    Created dimension tables

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -31,5 +31,44 @@ def main():
         print(f"Error connecting to MySQL database: {e}")
 
 
+class ETLPipeline:
+    
+    
+    def __init__(self):
+        self.name='next_cola_pipeline'
+        self.tables_dict = {}
+        
+        
+    def extract_data(self):
+        # Creating database cursor
+        cursor = connection.cursor()
+        
+        # Executing query to fetch all table names
+        cursor.execute('SHOW TABLES')
+        tables = cursor.fetchall()
+        
+        # Creating a dictionary for table name as key and dataframe as value by loop
+        for table in tables:
+            query = 'SELECT * FROM ' + table[0]
+            self.tables_dict[table[0]] = pd.read_sql(query, connection)
+            
+
+    def transform_data(self):
+        # Creating different data marts with their fact and dimensions tables (star schema)
+        
+        # First creating all the dimension tables
+        dim_supplier = self.tables_dict['supplier']
+        
+        dim_warehouse = self.tables_dict['warehouse'][['WarehouseID' ,'Capacity', 'OpeningHours', 'ClosingHours', 'ContactInfo']]
+        
+        dim_product = self.tables_dict['product'][['ProductID', 'Name', 'Price', 'StockLevel', 'ReorderLevel', 'Discontinued']]
+
+        dim_customer = self.tables_dict['customer']
+        
+        
 if __name__ == "__main__":
-    main()
+
+    pipeline = ETLPipeline()
+    pipeline.extract_data()
+    pipeline.transform_data()
+    
